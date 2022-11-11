65b9bc569c3b9768119bbe298aafb2f82cddd02e
Átila Delcanton Rampazo <CIANDT\atila.rampazo@lnb033961cps.ciandt.global>
2022-11-11 17:55:56 -0300

    andamento na instancia

--- do/init.sh
+++ do/init.sh
@@ -1,13 +1,19 @@
 !#/bin/bash
 
-sudo apt-get update -y;
-sudo DEBIAN_FRONTEND=noninteractive apt-get upgrade -y;
-sudo apt install apt-transport-https ca-certificates curl software-properties-common -y;
-curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
-sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu focal stable"
-apt-cache policy docker-ce
-sudo apt install docker-ce
-sudo systemctl status docker
-sudo usermod -aG docker ${USER}
-su - ${USER}
-sudo usermod -aG docker username
+"sudo apt update && apt  -y upgrade",
+"sudo DEBIAN_FRONTEND=noninteractive apt-get upgrade -y;
+"sudo apt install apt-transport-https ca-certificates curl software-properties-common -y;
+"curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -",
+"sudo add-apt-repository \"deb [arch=amd64] https://download.docker.com/linux/ubuntu focal stable\"",
+"sudo apt-cache policy docker-ce",
+"sudo apt update",
+"sudo apt install docker-ce",
+"sudo systemctl status docker",
+
+
+#security
+"ufw allow 22",
+"ufw allow 88",
+"ufw allow 443",
+"ufw --force enable",
+
--- do/main.tf
+++ do/main.tf
@@ -7,7 +7,7 @@ terraform {
   }
 }
 variable "api_token" {
-  default = "dop_v1_c7de7dfe07942616cd3a074ab7edfa91de66bde098af4266385a4672298bd93c"
+  default = "dop_v1_074e119dcac1d7b8b27009e571580da89c70c5e9fe4e5463e32c79d66a32f2ab"
 }
 variable "pvt_key" {
   default = "/home/atila.rampazo/.ssh/id_rsa"
@@ -21,10 +21,10 @@ data "digitalocean_ssh_key" "terraform" {
   name = "Ubunto Empresa"
 }
 resource "digitalocean_droplet" "web" {
-  image      = "ubuntu-22-10-x64"
-  name       = "web-1"
+  image      = "docker-20-04"
+  name       = "sonar-server"
   region     = "nyc3"
-  size       = "s-1vcpu-1gb"
+  size       = "s-2vcpu-4gb"
   monitoring = true
   ipv6       = true
   ssh_keys   = [data.digitalocean_ssh_key.terraform.fingerprint]
@@ -35,20 +35,30 @@ resource "digitalocean_droplet" "web" {
     private_key = file(var.pvt_key)
     timeout     = "2m"
   }
-  provisioner "remote-exec" {
-    inline = [
-      "export PATH=$PATH:/usr/bin",
-      "sudo apt-get update -y",
-      "sudo DEBIAN_FRONTEND=noninteractive apt-get upgrade -y",
-      "sudo apt install apt-transport-https ca-certificates curl software-properties-common -y",
-      "curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -",
-      "sudo add-apt-repository deb [arch=amd64] https://download.docker.com/linux/ubuntu focal stable",
-      "apt-cache policy docker-ce",
-      "sudo apt install docker-ce",
-      "sudo systemctl status docker",
-      "sudo usermod -aG docker ${USER}",
-      "su - ${USER}",
-      "sudo usermod -aG docker username"
-    ]
+  provisioner "file" {
+    source      = "docker-compose.yaml"
+    destination = "."
   }
+  /*provisioner "remote-exec" {
+    inline = [
+      #start docker containers
+      "docker volume create postgresql_data",
+      "docker volume create sonarqube_data",
+      "docker volume create sonarqube_extensions",
+      "docker volume create sonarqube_logs",
+      "docker volume create postgresql",
+
+      "docker login --username atilarampazo --password kr3m0g3m4",
+      "docker run --name sonarqubedb -p 5432:5432 -v pgdata:/var/lib/postgresql/data -e POSTGRES_USER=sonar -e POSTGRES_PASSWORD=sonar -d postgres",
+      "docker run --link sonarqubedb:sonarqubedb --name sonarqube -p 9000:9000 -e SONARQUBE_JDBC_USERNAME=sonar -e SONARQUBE_JDBC_PASSWORD=sonar -e SONARQUBE_JDBC_URL=jdbc:postgresql://sonarqubedb/sonar -d sonarqube",
+
+
+
+      #security
+      "ufw allow 22",
+      "ufw allow 88",
+      "ufw allow 443",
+      "ufw --force enable",
+    ]
+  }*/
 }
