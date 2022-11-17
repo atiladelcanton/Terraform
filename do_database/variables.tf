d8cc31346ea36400852b360584206ca2064b0c69
Atila Rampazo <atilarampazo@gmail.com>
2022-11-16 23:20:31 -0300

    create labs terraform main

--- do_database/variables.tf
+++ do_database/variables.tf
@@ -15,7 +15,7 @@ variable "size" {
   default = "db-s-1vcpu-2gb"
 }
 variable "api_token" {
-  default = "dop_v1_cf39993e9dd2fa93ab31b7531c6a604fd2371469ff8994567d1074ebe82e6ece"
+  default = ""
 }
 variable "pvt_key" {
   default = "~/.ssh/id_rsa"
--- do_mentoria/variables.tf
+++ do_mentoria/variables.tf
@@ -12,7 +12,7 @@ variable "ssh_user" {
   default = "root"
 }
 variable "api_token" {
-  default = "dop_v1_cf39993e9dd2fa93ab31b7531c6a604fd2371469ff8994567d1074ebe82e6ece"
+  default = ""
 }
 variable "pvt_key" {
   default = "~/.ssh/id_rsa"
